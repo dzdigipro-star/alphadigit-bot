44ede2304b8c64148997f40a52719a37975b755c
dzdigipro-star <dzdigipro@gmail.com>
2026-02-10 01:02:37 +0100

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -83,7 +83,7 @@ function createBot(token) {
             `/pay - Add funds to your account\n` +
             `/orders - View your orders\n` +
             `/help - Show this help message\n\n` +
-            `For support, contact the admin @seven_alfa`,
+            `For support, contact the admin @seven_alfa .`,
             { parse_mode: 'Markdown' }
         );
     });
